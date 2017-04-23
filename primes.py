ae92069727d04538710b8b6f339449b8ec19cd30
eliecerUribe <eliecer085@gmail.com>
2017-04-23 13:03:01 -0400

    py1

--- primes.py
+++ primes.py
@@ -8,19 +8,17 @@ class PrimeClass(object):
     Operations for prime numbers
     """
 
-    def __init__(self, number):
-        self.num = number
-
     def is_prime(self, num_int):
         """function(a) -> boolean"""
-        self.num = num_int
-        if self.num < 2:
+        if num_int < 2:
             return False
-        if self.num % 2 == 0:
+        if num_int % 2 == 0:
             return False
         acum = 3
-        while acum < self.num:
-            if self.num % acum == 0:
+        while acum < num_int:
+            if num_int % acum == 0:
                 return False
             acum += 2
         return True
+x = PrimeClass()
+print x.is_prime(2347)
